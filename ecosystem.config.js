4ad0f054c2a43ff75a9311bedb6f6824073a8b81
xodud1202 <xodud1202@naver.com>
2025-12-27 22:42:44 +0900

    pm2 기동 포트 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,11 @@ module.exports = {
             script: "npm",
             args: "start",
             autorestart: false,
-            max_memory_restart: "200M"
+            max_memory_restart: "300M",
+            env: {
+              PORT: 3013, // 여기에 포트를 명시합니다.
+              NODE_ENV: "production"
+            }
         }
     ]
 }
