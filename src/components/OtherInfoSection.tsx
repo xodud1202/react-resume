4355a47bdaec2e8d90d64a7d44536f8840c0bc71
xodud1202 <xodud1202@naver.com>
2026-01-26 17:59:57 +0900

    기타 경력 설명 줄바꿈 정상 동작 처리

--- src/components/OtherInfoSection.tsx
+++ src/components/OtherInfoSection.tsx
@@ -13,22 +13,22 @@ interface OtherInfoSectionProps {
     resumeOtherExperienceList?: resumeOtherExperience[]
 }
 
-const OtherInfoSection = ({resumeOtherExperienceList}: OtherInfoSectionProps) => {
+const OtherInfoSection = ({ resumeOtherExperienceList }: OtherInfoSectionProps) => {
     return (
         <section className="pdf-section">
             <h2 className="pdf-section-title">기타 경력 및 자격</h2>
             <div className="pdf-other">
                 {resumeOtherExperienceList?.map((item, idx) => (
-                <div className="pdf-other-item" key={idx}>
-                    <div className="pdf-other-header">
-                        <h3 className="pdf-other-title">{item.experienceTitle}</h3>
-                        <p className="pdf-other-period">{item.experienceStartDt} - {item.experienceEndDt}</p>
+                    <div className="pdf-other-item" key={idx}>
+                        <div className="pdf-other-header">
+                            <h3 className="pdf-other-title">{item.experienceTitle}</h3>
+                            <p className="pdf-other-period">{item.experienceStartDt} - {item.experienceEndDt}</p>
+                        </div>
+                        {item?.experienceSubTitle && (
+                            <p className="pdf-other-position">{item.experienceSubTitle}</p>
+                        )}
+                        <p className="pdf-other-desc whitespace-pre-wrap">{item.experienceDesc}</p>
                     </div>
-                    {item?.experienceSubTitle && (
-                        <p className="pdf-other-position">{item.experienceSubTitle}</p>
-                    )}
-                    <p className="pdf-other-desc">{item.experienceDesc}</p>
-                </div>
                 ))}
             </div>
 
